2fdb6cadc9b4901805e90dba0f2616da16b41ceb
Tomek Buslowski <tbuslowski@gmail.com>
2019-11-03 23:39:23 +0100

    Task 3 done: fetching GitHub user info and displaying

---
+++ src/components/MyProfile.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const UserName = styled.div`
+  font-family: TTCommons-regular, sans-serif;
+  font-size: 32px;
+  font-weight: bold;
+  padding-bottom: 24px;
+`
+
+export const GitHubAvatar = styled.img`
+  width: 300px;
+  height: 300px;
+  
+  border-radius: 8px;
+`
--- src/pages/MyProfile/index.js
+++ src/pages/MyProfile/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import * as Section from 'components/PageSection'
+import {UserName, GitHubAvatar} from 'components/MyProfile'
+
+const axios = require('axios')
 
 export const MyProfile = () => {
+  
+  const [user, setUser] = useState(null)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://api.github.com/users/sandfrozen')
+      setUser(result.data)
+    }
+    fetchData()
+  }, [])
   
   return (
     <Section.Content>
       <Section.Header>
-        <Section.Title>My Profile</Section.Title>
+        <UserName>{user ? user.name : 'Loading...'}</UserName>
+        {user &&
+        <Section.Description>
+          <GitHubAvatar src={user.avatar_url}/>
+        </Section.Description>}
       </Section.Header>
     </Section.Content>
   )
 }
+
