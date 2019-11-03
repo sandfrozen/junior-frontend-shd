5669e70011d4a829a704364ed7dfbddf9fdd607f
Tomek Buslowski <tbuslowski@gmail.com>
2019-11-03 18:54:15 +0100

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Root, Wrapper, Logo } from "./styles";
+
+export const Header = () => (
+  <Root>
+    <Wrapper>
+      <Logo />
+    </Wrapper>
+  </Root>
+);
